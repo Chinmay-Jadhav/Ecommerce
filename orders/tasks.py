7db0502bb0acb0561312a7cf6d259fdd73bc8e0c
Chinmay Jadhav <chinmayjadhav2003@gmail.com>
2026-08-28 12:26:01 +0530

    convert decimal to json serializable

--- orders/tasks.py
+++ orders/tasks.py
@@ -4,9 +4,22 @@ from celery import shared_task
 
 import time
 import requests
+import json
+import decimal
 
 from .models import Order
 from payment_gateway.constants import PAYMENT_GATEWAY_PATH
+
+class NumberStr(float) : 
+    def __init__(self, o):
+        self.o = o
+    def __repr__(self):
+        return str(self.o)
+
+def decimal_serializer(o) : 
+    if isinstance(o, decimal.Decimal) : 
+        return NumberStr(o)
+    raise TypeError(f"{repr(o)} is not JSON serializable")
 
 @shared_task
 def process_order(order_id : int)  :
@@ -14,13 +27,22 @@ def process_order(order_id : int)  :
 
     process_payload = {
             "order_id" : order_id ,
-            "amount" : str(order.total_price) ,  
+            "amount" : order.total_price ,  
             "payment_method" : order.payment_method.name ,
         }
 
+    payload = json.dumps(
+        process_payload,
+        default=decimal_serializer,
+    )
+
     process_response = requests.post(
         f"{settings.BASE_URL}{PAYMENT_GATEWAY_PATH}",
-        json=process_payload,
+        data=payload,
+        headers={
+            'Content-Type': 'application/json',
+            'Accept' : 'application/json'
+            },
     )
 
     process_response.raise_for_status()
